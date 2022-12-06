6ffb54a2aeddd32e971f55dbfef58aa1402c5f60
febiasm <trevorfebias@gmail.com>
2022-12-06 20:18:34 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
+
+ 
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector("#create-task-form");
+ 
+  const renderTodoItem = function (e) {
+    e.preventDefault();
+    const todoItemInput = document.querySelector("#new-task-description").value;
+    const todoList = document.querySelector("#tasks");
+    const item = document.createElement("li");
+    item.textContent = todoItemInput;
+ 
+    todoList.prepend(item);
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerHTML = "Delete";
+    item.append(deleteBtn);
+    const deleteTodo = function () {
+      item.remove()
+    };
+    deleteBtn.addEventListener("click", deleteTodo);
+  };
+
+  form.addEventListener("submit", renderTodoItem);
 });
+
+
